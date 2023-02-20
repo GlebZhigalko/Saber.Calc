5cd411276e14474f110ec309d4b69a4abd6aae0f
GlebZhigalko <117540063+GlebZhigalko@users.noreply.github.com>
2023-02-20 03:48:21 +0300

    Update Saber.Calc.py
    >>Created buttons and their connection with the function
    >>Созданы кнопки и их связь с функцией

--- Saber.Calc.py
+++ Saber.Calc.py
@@ -1,16 +1,40 @@
 import wx
 
 
-class MyFrame(wx.Frame):
+class MyFrame(wx.Frame):  # Создан класс с наследованием wx.Frame
     def __init__(self, parent, title):
         super().__init__(parent, title=title, size=(300, 300))
-        panel = wx.Panel(self)
+        panel = wx.Panel(self)  # Создана панель
 
-        font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)  # Изменен размера шрифта
         font.SetPointSize(10)
         panel.SetFont(font)
 
-        vbox = wx.BoxSizer(wx.VERTICAL)
-        self.txtCtrl = wx.ComboBox(panel)
-
+        vbox = wx.BoxSizer(wx.VERTICAL)  # Создан вертикальный сайзер
+        self.txtCtrl = wx.ComboBox(panel)  # Создано поле ввода
         vbox.Add(self.txtCtrl, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP, border=10)
+
+        gbox = wx.GridSizer(5, 4, 5, 5)
+        gbox.AddMany([(wx.Button(panel, label='C'), wx.ID_ANY, wx.EXPAND),  # Создание кнопок
+                      (wx.StaticText(panel), wx.EXPAND),
+                      (wx.Button(panel, label='<-'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='Close'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='7'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='8'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='9'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='/'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='4'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='5'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='6'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='*'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='1'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='2'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='3'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='-'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='0'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='.'), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='='), wx.ID_ANY, wx.EXPAND),
+                      (wx.Button(panel, label='+'), wx.ID_ANY, wx.EXPAND)])
+        vbox.Add(gbox, proportion=1, flag=wx.EXPAND | wx.ALL, border=10)
+        panel.SetSizer(vbox)
+        self.Bind(wx.EVT_BUTTON, self.OnClicked)  # Создана связь кнопка c функцией
